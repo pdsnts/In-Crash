548634b3a0e4f61f33978b6a155c8e27ff4fa390
Pedro Roberto Santos de Lima <nig0195017@aluno.firjansenaisesi.com.br>
2026-08-25 14:27:02 -0300

    Adiciona variavel float

--- Assets/scripts/carMoviment.cs
+++ Assets/scripts/carMoviment.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public string "GitHubDoMaisChadVulgoDavi";
-    public int = 12
-    public int = 4
+    public int = 12;
+    public int = 4;
+    public float = 2;
 }
