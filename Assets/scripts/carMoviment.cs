89d1ec49415b9a75c5af290d0c61b78554757db4
Pedro Roberto Santos de Lima <nig0195017@aluno.firjansenaisesi.com.br>
2026-08-25 14:54:36 -0300

    feat: Adiciona variacoes de velocidade

--- Assets/scripts/carMoviment.cs
+++ Assets/scripts/carMoviment.cs
@@ -5,9 +5,11 @@ public class Player : MonoBehaviour
     public string "GitHubDoMaisChadVulgoDavi";
     public int = 12;
     public int = 4;
+    public float velocity = 3.2f;
     public float = 2;
     public string "AulaGitHubRenatoVictor";
     public int = 10;
     public float = 4.1f;
     Vector3;
-}
+    Vector2 * velocity;
+}  
