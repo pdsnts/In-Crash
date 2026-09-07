40eff19df2b2ac1dd73f2d55dac592c49f6bff0f
Pedro <pdsantos.stack@gmail.com>
2026-09-06 19:06:56 -0700

    Add buttons script and quit button

--- Assets/scripts/SceneChanger.cs
+++ Assets/scripts/SceneChanger.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Serialization;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string GameScene;
+    [SerializeField] private GameObject MainMenuPanel;
+    [SerializeField] private GameObject OptionsPanel;
+
+    public void Play()
     {
-        
+        SceneManager.LoadScene(GameScene);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenOptions()
     {
-        
+        MainMenuPanel.SetActive(false);
+        OptionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        OptionsPanel.SetActive(false);
+        MainMenuPanel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        UnityEngine.Debug.Log("Sair do jogo");
+        Application.Quit();
     }
 }
